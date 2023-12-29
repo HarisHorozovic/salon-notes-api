330ac6f34a92fa28543bc048d9261b8d27d76955
Haris Horozovic <haris@balkon.ba>
2023-12-29 19:26:59 +0100

    Sanity logs

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const { PORT } = require("./config");
-const { getDatabaseConnection, create } = require("./db");
+const { PORT, DATABASE_URL } = require("./config");
+const { getDatabaseConnection, findOne } = require("./db");
 
 const port = PORT || 3001;
 const app = express();
 app.use(express.json());
 
-create().then();
+console.log(
+  "____________________________________________________________________________________"
+);
+console.log(DATABASE_URL);
+console.log(
+  "____________________________________________________________________________________"
+);
+findOne().then();
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello App" });
 });
