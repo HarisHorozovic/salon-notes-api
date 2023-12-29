ffd2001d77cba2a28a04c8402b7d83e4954bf6ad
Haris Horozovic <haris@balkon.ba>
2023-12-29 19:24:05 +0100

    Test mongodb connection

---
+++ db/index.js
@@ -0,0 +1,39 @@
+const { DATABASE_URL } = require("../config");
+const mongodb = require("mongodb").MongoClient;
+
+exports.create = async (db, collection, data) => {
+  const connection = await mongodb.connect(DATABASE_URL);
+  //   Do stuff
+  await connection.close();
+};
+
+exports.findOne = async (db, collection) => {
+  const connection = await mongodb.connect(DATABASE_URL);
+  console.log(
+    "____________________________________________________________________________________"
+  );
+  console.log(connection);
+  console.log(
+    "____________________________________________________________________________________"
+  );
+  //   Do stuff
+  await connection.close();
+};
+
+exports.find = async (db, collection, query) => {
+  const connection = await mongodb.connect(DATABASE_URL);
+  //   Do stuff
+  await connection.close();
+};
+
+exports.update = async (db, collection, query, data) => {
+  const connection = await mongodb.connect(DATABASE_URL);
+  //   Do stuff
+  await connection.close();
+};
+
+exports.delete = async (db, collection, query) => {
+  const connection = await mongodb.connect(DATABASE_URL);
+  //   Do stuff
+  await connection.close();
+};
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-require("dotenv").config();
+const { PORT } = require("./config");
+const { getDatabaseConnection, create } = require("./db");
 
-const port = process.env.PORT || 3001;
+const port = PORT || 3001;
 const app = express();
 app.use(express.json());
 
+create().then();
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello App" });
 });
